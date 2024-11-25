fc6ac1431c3e5abcff341d6cfc81e2849a135213
anonymous12220 <vulanvi22@gmail.com>
2024-11-25 19:06:19 +0700

    first commit

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .networks_2d.xnet import XNet_sb
+from .networks_2d.unet import unet, r2_unet, attention_unet
+from .networks_2d.unet_plusplus import unet_plusplus
+from .networks_2d.swinunet import swinunet
+from .networks_2d.resunet import res_unet
+from .networks_2d.resunet_plusplus import res_unet_plusplus
